ee5995e7abea61e3dc72a2fbc1ff51824292e58c
fidel <fideljose1708@hotmail.com>
2023-11-26 03:50:17 -0500

    Add currying code test

--- src/main/java/com/fideljose/trainingStreamJava/functional/Main.java
+++ src/main/java/com/fideljose/trainingStreamJava/functional/Main.java
@@ -1,5 +1,6 @@
 package com.fideljose.trainingStreamJava.functional;
 
+import java.util.Locale;
 import java.util.function.Function;
 
 public class Main {
@@ -17,16 +18,33 @@ public class Main {
         myFunctionalInterface.myFunction();
 
         // Chaining Functions
-        Function<Integer, Integer> f1 = (n) -> n*7;
-        Function<Integer, Integer> f2 = (r) -> r-17;
-        Function<Integer, Integer> f3 = f1.andThen(f2);
-        System.out.println(f3.apply(10));
+//        Function<Integer, Integer> f1 = (n) -> n*7;
+//        Function<Integer, Integer> f2 = (r) -> r-17;
+//        Function<Integer, Integer> f3 = f1.andThen(f2);
+//        System.out.println(f3.apply(10));
+//
+//        // Compose Functions - inverter chaining order
+//        Function<Integer, Integer> f1a = (n) -> n*7;
+//        Function<Integer, Integer> f2a = (r) -> r-17;
+//        Function<Integer, Integer> f3a = f1a.compose(f2a);
+//        System.out.println(f3a.apply(10));
 
-        // Compose Functions - inverter chaining order
-        Function<Integer, Integer> f1a = (n) -> n*7;
-        Function<Integer, Integer> f2a = (r) -> r-17;
-        Function<Integer, Integer> f3a = f1a.compose(f2a);
-        System.out.println(f3a.apply(10));
+        // Currying
+        Function<Integer, Function<Integer, Function<Integer, Integer>>> fun1= f1 -> f2 -> f3 -> f1 + f2 - f3;
+        Function<Integer, Function<Integer, Integer>> fun2 = fun1.apply(2);
+        Function<Integer, Integer> fuct3 = fun2.apply(3);
+        Integer res = fuct3.apply(0);
+        System.out.println(res);
+
+        // Currying example 2
+        Function<String, Function<String, Function<String, String>>> fun = string1 -> string2 -> string3 -> string1.toString().concat("-Fidel")
+                                                                                                                + string2.toUpperCase()
+                                                                                                                + string3.isEmpty() ;
+        Function<String, Function<String, String>> f2 = fun.apply("valor inicial");
+        Function<String, String> f3 = f2.apply(" - valor 2 - ");
+        String result = f3.apply("Valor 3");
+        System.out.println(result);
+
     }
 
 
